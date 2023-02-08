97265412f5343881320d34272bb67721fd33ea49
Andrei Trunkin <inboxavt@gmail.com>
2023-02-08 22:20:07 +0100

    updated attributes of cells

--- scrypt.js
+++ scrypt.js
@@ -1,13 +1,14 @@
 const createField = () => {
   const tableBox = document.createElement('table');
   tableBox.classList.add('game');
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 1; i < 4; i += 1) {
     const str = tableBox.insertRow();
     str.classList.add('line');
-    for (let j = 0; j < 3; j += 1) {
+    for (let j = 1; j < 4; j += 1) {
       const cell = str.insertCell();
       cell.classList.add('cell');
-      cell.setAttribute('cellNum', j);
+      const cellNum = `${i}.${j}`;
+      cell.setAttribute('cellNum', cellNum);
       // console.log(cell);
     }
   }
