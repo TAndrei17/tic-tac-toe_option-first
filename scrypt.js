acb8833fcd33f1c91ffb37df4bcb07432a530361
Andrei Trunkin <inboxavt@gmail.com>
2023-02-12 21:13:27 +0100

    added step and button reload

--- scrypt.js
+++ scrypt.js
@@ -5,10 +5,13 @@ const goToPlay = () => {
   const tableAll = createField();
   const divRoot = document.querySelector('.root');
   divRoot.append(tableAll);
+  const turn = document.querySelector('.step');
   let cellText = 'X';
+  document.body.prepend(turn);
   // here attributes of every cell is collected (steps collection)
   const allXs = [];
   const allOs = [];
+  turn.textContent = `${cellText} plays`;
   tableAll.addEventListener('click', (event) => {
     const { target } = event;
     const getCellNum = event.target.getAttribute('cellNum');
@@ -20,6 +23,7 @@ const goToPlay = () => {
     if (target.textContent === 'X' || target.textContent === 'O') {
       cellText = cellText === 'X' ? 'O' : 'X';
     }
+    turn.textContent = `${cellText} plays`;
     if ((target.textContent === 'X') && (!allXs.includes(getCellNum))) {
       const newX = target.getAttribute('cellNum');
       // collect every step of 'X'
@@ -34,34 +38,61 @@ const goToPlay = () => {
     const hasWinnerX = isWinner(allXs);
     const hasWinnerXDiagonal = isWinnerDiagonal(allXs);
     if ((hasWinnerX) || (hasWinnerXDiagonal)) {
+      turn.remove();
       // if winner appears is created <div> with class '.winner'
       const divWinner = document.createElement('div');
-      const text = document.createTextNode('Congrats! X is winner!');
-      divWinner.append(text);
+      const paragraph = document.createElement('p');
+      const text = document.createTextNode('CONGRATS! \'X\' IS WINNER!');
+      paragraph.append(text);
+      divWinner.append(paragraph);
+      const button = document.createElement('button');
+      const textButton = document.createTextNode('START AGAIN');
+      button.append(textButton);
+      divWinner.append(button);
       divRoot.classList.add('winner');
       document.body.append(divWinner);
       tableAll.replaceWith(divWinner);
+      const butt = document.querySelector('div > button');
+      butt.addEventListener('click', () => document.location.reload());
     }
     // check O's collection on every step
     const hasWinnerO = isWinner(allOs);
     const hasWinnerODiagonal = isWinnerDiagonal(allOs);
     if ((hasWinnerO) || (hasWinnerODiagonal)) {
+      turn.remove();
       // if winner appears is created <div> with class '.winner'
       const divWinner = document.createElement('div');
-      const text = document.createTextNode('Congrats! O is winner!');
-      divWinner.append(text);
+      const paragraph = document.createElement('p');
+      const text = document.createTextNode('CONGRATS! \'O\' IS WINNER!');
+      paragraph.append(text);
+      divWinner.append(paragraph);
+      const button = document.createElement('button');
+      const textButton = document.createTextNode('START AGAIN');
+      button.append(textButton);
+      divWinner.append(button);
       divRoot.classList.add('winner');
       document.body.append(divWinner);
       tableAll.replaceWith(divWinner);
+      const butt = document.querySelector('div > button');
+      butt.addEventListener('click', () => document.location.reload());
     }
     const getWinner = document.querySelector('.winner');
     if (([...allXs, ...allOs].length === 9) && (!getWinner)) {
+      turn.remove();
       const divWinner = document.createElement('div');
-      const text = document.createTextNode('Draw!');
-      divWinner.append(text);
+      const paragraph = document.createElement('p');
+      const text = document.createTextNode('DRAW!');
+      paragraph.append(text);
+      divWinner.append(paragraph);
+      const button = document.createElement('button');
+      const textButton = document.createTextNode('START AGAIN');
+      button.append(textButton);
+      divWinner.append(button);
       divRoot.classList.add('winner');
       document.body.append(divWinner);
       tableAll.replaceWith(divWinner);
+      const butt = document.querySelector('div > button');
+      butt.addEventListener('click', () => document.location.reload());
     }
   });
 };
